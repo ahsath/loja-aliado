afa4d56a34c79257e770f8e156ae3863b793196c
Ahsath <ahsath@gmail.com>
2022-04-01 21:34:48 +0100

    feat: add new route views and layouts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
+// Onboarding views
 export { default as Home } from './Home';
 export { default as Welcome } from './Welcome';
 
-// Store views
-export { default as Store } from './store/Store';
+// Dashboard view
+export { default as Dashboard } from './dashboard/Dashboard';
+
+// Orders views
+export { default as Orders } from './orders/Orders';
+
+// Store views & layouts
+export { default as StoreLayout } from './store/layouts/StoreLayout';
 export { default as StoreProducts } from './store/StoreProducts';
 export { default as StoreProfile } from './store/settings/profile/StoreProfile';

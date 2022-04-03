84bb5f813ca4a62687ecba9ae55f86b9b09256a2
Ahsath <ahsath@gmail.com>
2022-04-03 20:53:41 +0100

    refactor: rename component

--- src/layouts/app-layout/components/BottomNav.jsx
+++ src/layouts/app-layout/components/BottomNav.jsx
@@ -1,17 +1,12 @@
-import {
-  BottomNavigation,
-  BottomNavigationAction,
-  Avatar,
-  Box,
-} from '@mui/material';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import DashboardRounded from '@mui/icons-material/DashboardRounded';
 import ReceiptRounded from '@mui/icons-material/ReceiptRounded';
 import StoreRounded from '@mui/icons-material/StoreRounded';
-import avatar from '../../../assets/img/avatar.jpg';
+
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-export default function AppNav() {
+export default function BottomNav() {
   const { pathname } = useLocation();
   const [currentPath, setCurrentPath] = useState(pathname);
 
@@ -26,7 +21,14 @@ export default function AppNav() {
     <BottomNavigation
       value={currentPath}
       component="nav"
-      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        borderTop: 'solid',
+        borderColor: 'divider',
+      }}
       onChange={(_, path) => setCurrentPath(path)}
       showLabels
     >
@@ -41,9 +43,6 @@ export default function AppNav() {
           />
         );
       })}
-      <Box className="flex items-center justify-center grow px-3">
-        <Avatar src={avatar} sx={{ width: 36, height: 36 }} />
-      </Box>
     </BottomNavigation>
   );
 }

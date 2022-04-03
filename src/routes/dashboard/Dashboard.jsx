205777e39ef02c0b1220892ef4ce4313cda6dbae
Ahsath <ahsath@gmail.com>
2022-04-03 03:39:32 +0100

    feat: use `BoardOrdersBook`

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,9 +1,14 @@
 import BoardIncome from './components/BoardIncome';
+import BoardOrdersBook from './components/BoardOrdersBook';
+import newOrders from './components/newOrders.json';
+import prevOrders from './components/prevOrders.json';
 
 export default function Dashboard() {
   return (
-    <div className="p-4">
+    <div className="grid gap-2 py-2">
       <BoardIncome />
+      <BoardOrdersBook title="Novas ordens" orders={newOrders} ring />
+      <BoardOrdersBook title="Ordens anteriores" orders={prevOrders} />
     </div>
   );
 }

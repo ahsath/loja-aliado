08f5de89a898e021eded43f26c04cb002da558de
Ahsath <ahsath@gmail.com>
2022-04-03 20:54:03 +0100

    refactor: update `AppLayout.jsx`

--- src/layouts/app-layout/AppLayout.jsx
+++ src/layouts/app-layout/AppLayout.jsx
@@ -1,11 +1,13 @@
-import AppNav from './components/AppNav';
 import { Outlet } from 'react-router-dom';
+import BottomNav from './components/BottomNav';
+import TopAppBar from './components/TopAppBar';
 
 export default function AppLayout() {
   return (
     <>
+      <TopAppBar />
       <Outlet />
-      <AppNav />
+      <BottomNav />
     </>
   );
 }

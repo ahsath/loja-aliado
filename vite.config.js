830a51183e15fd191f30ebc099b72c1c3d320ae4
Ahsath <ahsath@gmail.com>
2022-04-04 21:19:22 +0100

    feat(viteConfig): add unplugin api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({
+      resolvers: [
+        IconsResolver({
+          prefix: 'Icon',
+          extension: 'jsx',
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+      compiler: 'jsx',
+      jsx: 'react',
+      iconCustomizer(_c, _i, props) {
+        props.width = '24';
+        props.height = '24';
+      },
+    }),
+  ],
 });

148c8442aaf5d5ecac5d3a1c00d69af8af10dc20
Ahsath <ahsath@gmail.com>
2022-04-06 00:42:12 +0100

    refactor(Dashboard.jsx): replace Box for Main component

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,20 +1,20 @@
-import { Box } from '@mui/material';
 import BoardIncome from './components/BoardIncome';
 import BoardOrdersBook from './components/BoardOrdersBook';
 import BoardLearn from './components/BoardLearn';
 
 import newOrders from './components/newOrders.json';
 import prevOrders from './components/prevOrders.json';
+import { Main } from '../../components';
 
 export default function Dashboard() {
   return (
-    <Box className="py-16" sx={{ pt: { sm: 9 } }}>
+    <Main>
       <div className="grid gap-2">
         <BoardIncome />
         <BoardOrdersBook title="Novas ordens" orders={newOrders} ring />
         <BoardOrdersBook title="Ordens anteriores" orders={prevOrders} />
         <BoardLearn />
       </div>
-    </Box>
+    </Main>
   );
 }

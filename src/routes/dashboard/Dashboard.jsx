cf85b52d86b8dfd61c950729fc413dffd96f7549
Ahsath <ahsath@gmail.com>
2022-04-05 11:54:21 +0100

    feat(Dashboard.jsx): update padding-top on sm breakpoint

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,14 +1,20 @@
+import { Box } from '@mui/material';
 import BoardIncome from './components/BoardIncome';
 import BoardOrdersBook from './components/BoardOrdersBook';
+import BoardLearn from './components/BoardLearn';
+
 import newOrders from './components/newOrders.json';
 import prevOrders from './components/prevOrders.json';
 
 export default function Dashboard() {
   return (
-    <div className="grid gap-2 py-2">
-      <BoardIncome />
-      <BoardOrdersBook title="Novas ordens" orders={newOrders} ring />
-      <BoardOrdersBook title="Ordens anteriores" orders={prevOrders} />
-    </div>
+    <Box className="py-16" sx={{ pt: { sm: 9 } }}>
+      <div className="grid gap-2">
+        <BoardIncome />
+        <BoardOrdersBook title="Novas ordens" orders={newOrders} ring />
+        <BoardOrdersBook title="Ordens anteriores" orders={prevOrders} />
+        <BoardLearn />
+      </div>
+    </Box>
   );
 }

837155e65cb87b9a470cf71b29e7a3f946d77059
Ahsath <ahsath@gmail.com>
2022-04-05 11:52:27 +0100

    refactor: rename component import name

--- src/layouts/app-layout/components/top-appbar/TopAppBar.jsx
+++ src/layouts/app-layout/components/top-appbar/TopAppBar.jsx
@@ -1,11 +1,11 @@
 import { AppBar, Box, Toolbar, IconButton, Avatar } from '@mui/material';
 import avatar from '../../../../assets/img/avatar.jpg';
-import OpenCloseToggleButton from './OpenCloseStoreToggleButton';
+import ToggleStoreButton from './ToggleStoreButton';
 
 export default function TopAppBar() {
   return (
     <AppBar
-      position="sticky"
+      position="fixed"
       elevation={0}
       sx={{
         backgroundColor: 'background.paper',
@@ -22,7 +22,7 @@ export default function TopAppBar() {
           <div className="font-black text-xs">Aliado</div>
         </Box>
         <div>
-          <OpenCloseToggleButton />
+          <ToggleStoreButton />
           <IconButton sx={{ mr: -1 }}>
             <Avatar src={avatar} sx={{ width: 32, height: 32 }} />
           </IconButton>

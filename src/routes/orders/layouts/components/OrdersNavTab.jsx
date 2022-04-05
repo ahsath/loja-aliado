66bcf4ca42f435fcf524049678b8615f530356f8
Ahsath <ahsath@gmail.com>
2022-04-06 00:42:52 +0100

    feat(OrdersNavTab.jsx): add component

---
+++ src/routes/orders/layouts/components/OrdersNavTab.jsx
@@ -0,0 +1,36 @@
+import { Tabs, Tab, Slide, useScrollTrigger } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+export default function OrdersNavTab() {
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      <Tabs
+        value={useLocation().pathname}
+        sx={{
+          bgcolor: (theme) => theme.palette.background.paper,
+          position: 'sticky',
+          top: { xs: 56, sm: 64 },
+        }}
+        variant="fullWidth"
+        aria-label="lab API tabs example"
+      >
+        <Tab label="Novas" value="/orders" to="/orders" component={Link} />
+        <Tab
+          label="No curso"
+          value="/orders/ongoing"
+          to="ongoing"
+          component={Link}
+        />
+        <Tab
+          label="Anteriores"
+          value="/orders/previous"
+          to="previous"
+          component={Link}
+        />
+      </Tabs>
+    </Slide>
+  );
+}

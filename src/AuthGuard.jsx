81c0264c9ab9c1a2eefe100958cf7a2d3615303d
Ahsath <ahsath@gmail.com>
2022-04-17 10:12:05 +0100

    feat(AuthGuard.jsx): navigate away if there is no realm user

--- src/AuthGuard.jsx
+++ src/AuthGuard.jsx
@@ -1,11 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Outlet, Navigate } from 'react-router-dom';
+import useRealm from '../hooks/useRealm';
 
 export default function AuthGuard() {
   const { user, isLoading } = useAuth0();
+  const { currentUser } = useRealm();
 
   if (isLoading) return <div>loading...</div>;
-  if (!user) return <Navigate to="/" />;
+  if (!user || !currentUser) return <Navigate to="/" />;
 
   return <Outlet />;
 }

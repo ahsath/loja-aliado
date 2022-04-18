dd1c9cc2269c3d9c5319d622e679655586e566f4
Ahsath <ahsath@gmail.com>
2022-04-19 00:14:30 +0100

    refactor(MapboxProvider.jsx): expose map methods instead of creating map

--- src/components/mapbox/MapboxProvider.jsx
+++ src/components/mapbox/MapboxProvider.jsx
@@ -1,23 +1,32 @@
-import { createContext, useEffect, useState } from 'react';
+import { useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { MapboxMapContext } from './context';
 
 export default function MapboxProvider({ accessToken, children }) {
   mapboxgl.accessToken = accessToken;
-  const [map, setMap] = useState();
 
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-    });
-
-    setMap(map);
-  }, []);
+  const [mapbox] = useState({
+    map(container) {
+      return new mapboxgl.Map({
+        container,
+        style: 'mapbox://styles/mapbox/streets-v11',
+      });
+    },
+    marker(element) {
+      return new mapboxgl.Marker({ element });
+    },
+    geolocateControl() {
+      return new mapboxgl.GeolocateControl({
+        trackUserLocation: true,
+        showUserHeading: true,
+        positionOptions: { enableHighAccuracy: true },
+      });
+    },
+  });
 
   return (
-    <MapboxMapContext.Provider value={{ map, mapboxgl }}>
+    <MapboxMapContext.Provider value={{ ...mapbox }}>
       {children}
     </MapboxMapContext.Provider>
   );

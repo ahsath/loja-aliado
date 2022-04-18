14ddcac0d8ade6113009887a061f98dfac77d425
Ahsath <ahsath@gmail.com>
2022-04-19 00:14:56 +0100

    refactor(StoreProfile.jsx): update component markup

--- src/routes/store/settings/profile/StoreProfile.jsx
+++ src/routes/store/settings/profile/StoreProfile.jsx
@@ -1,72 +1,120 @@
-import { Badge, Avatar, Box, TextField, Button } from '@mui/material';
-import {
-  MapboxProvider,
-  MapboxMap,
-  MapboxMarker,
-  MapboxPin,
-  UploadFileButton,
-} from '../../../../components';
-import { useState } from 'react';
+import { Badge, Avatar, Box, TextField, Button, Toolbar } from '@mui/material';
+import { UploadFileButton, Page } from '../../../../components';
+import ArrowBackRounded from '@mui/icons-material/ArrowBackRounded';
+import SaveRounded from '@mui/icons-material/SaveRounded';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import useMapbox from '../../../../components/mapbox/useMapbox';
 
 export default function StoreSettingsProfile() {
-  const [lngLat, setLngLat] = useState([0, 0]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const mapbox = useMapbox();
 
-  const onMove = ({ target: map }) => {
-    const { lng, lat } = map.getCenter();
-    setLngLat([lng, lat]);
-  };
+  useEffect(() => {
+    const map = mapbox.map('map');
+    const geolocateControl = mapbox.geolocateControl();
+    map.addControl(geolocateControl);
+    // const marker = mapbox.marker()
+
+    map.on('move', ({ target }) => {
+      const { lng, lat } = target.getCenter();
+      console.log(lng, lat);
+    });
+
+    map.on('load', () => {
+      geolocateControl.trigger();
+    });
+
+    geolocateControl.on('error', (data) => {
+      console.log(data);
+    });
+
+    return () => map.remove();
+  }, []);
+
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="flex flex-col h-screen p-4">
-      <div className="flex gap-6 flex-col pb-6">
-        <Badge
-          className="self-center"
-          overlap="circular"
-          badgeContent={
-            <UploadFileButton ariaLabel="Toque para selecionar um logotipo para sua loja" />
-          }
-          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-        >
-          <Avatar
-            sx={{ width: 100, height: 100, bgcolor: 'primary.light' }}
-            alt="M logo"
-          >
-            <Box
-              component="span"
-              className="font-['Pacifico',_cursive]"
-              sx={{ color: 'primary.main' }}
-            >
-              M
-            </Box>
-          </Avatar>
-        </Badge>
-
-        <TextField label="Nome da empresa" size="small" fullWidth />
-        <TextField label="Pesquisar localização" size="small" fullWidth />
-      </div>
-
-      <div className="grid flex-grow">
-        <MapboxProvider accessToken={import.meta.env.VITE_ACCESS_TOKEN}>
-          <MapboxMap
-            className="-mx-4 -mb-4 col-span-full row-span-full"
-            onMove={onMove}
-          >
-            <MapboxMarker
-              options={{
-                element: <MapboxPin className="w-8 h-8" />,
-                lngLat,
-              }}
-            />
-          </MapboxMap>
-        </MapboxProvider>
+    <Page
+      title="Perfil da Loja"
+      backIcon={<ArrowBackRounded />}
+      bottomContent={
         <Button
-          className="w-full col-span-full row-span-full self-end z-10"
+          startIcon={<SaveRounded />}
           variant="contained"
-          size="large"
+          type="submit"
+          form="store-profile"
+          fullWidth
         >
-          Pronto
+          Salvar
         </Button>
+      }
+      spaceless
+    >
+      <div className="flex flex-col h-screen">
+        <Toolbar />
+        <form
+          id="store-profile"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col p-4"
+        >
+          <Badge
+            className="self-center"
+            overlap="circular"
+            badgeContent={
+              <UploadFileButton ariaLabel="Toque para selecionar um logotipo para sua loja" />
+            }
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          >
+            <Avatar
+              sx={{ width: 100, height: 100, bgcolor: 'primary.light' }}
+              alt="M logo"
+            >
+              <Box
+                component="span"
+                className="font-['Pacifico',_cursive]"
+                sx={{ color: 'primary.main' }}
+              >
+                M
+              </Box>
+            </Avatar>
+          </Badge>
+
+          <TextField
+            {...register('name', { required: true })}
+            form="store-profile"
+            label="Nome da empresa"
+            error={!!errors.name}
+            helperText={
+              errors.name?.type === 'required' && 'O nome da loja é obrigatório'
+            }
+            size="small"
+            autoComplete="off"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            {...register('location', {
+              required: true,
+            })}
+            label="Pesquisar localização"
+            autoComplete="off"
+            error={!!errors.location}
+            helperText={
+              errors.location?.type === 'required' &&
+              'A localização da loja é obrigatória'
+            }
+            size="small"
+            fullWidth
+            margin="normal"
+          />
+        </form>
+        <div id="map" className="grow" />
       </div>
-    </div>
+    </Page>
   );
 }

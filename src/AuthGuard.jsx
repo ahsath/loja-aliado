6f220fe9a9f489ce5d7b79a546d47181a36c87bf
Ahsath <ahsath@gmail.com>
2022-04-19 21:01:13 +0100

    refactor(AuthGuard.jsx): update useRealm import path

--- src/AuthGuard.jsx
+++ src/AuthGuard.jsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Outlet, Navigate } from 'react-router-dom';
-import useRealm from '../hooks/useRealm';
+import useRealm from './hooks/useRealm';
 
 export default function AuthGuard() {
   const { user, isLoading } = useAuth0();
